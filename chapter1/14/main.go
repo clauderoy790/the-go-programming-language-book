77ea32d2ff6c54eca4f2b8763f0994545539bf50
Claude Roy <claude.roy@mheducation.com>
2021-08-05 20:56:31 -0500

    resolve first problem

---
+++ chapter1/14/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+	for file, m := range counts {
+		for line, count := range m {
+
+		}
+		if count > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[f.Name()][input.Text()]++
+	}
+	// NOTE: ignoring potential errors from input.Err()
+}
