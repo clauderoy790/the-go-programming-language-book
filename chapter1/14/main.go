fb5c23f4c3d220b2854ca8ee6b73bba2306a9f86
Claude Roy <claude.roy@mheducation.com>
2021-08-06 07:59:51 -0500

    start ch1 exercises

--- chapter1/14/main.go
+++ chapter1/14/main.go
@@ -6,18 +6,21 @@ import (
 	"os"
 )
 
+//123
+//234
+//345
 func main() {
 	counts := make(map[string]map[string]int)
 	files := os.Args[1:]
 	if len(files) == 0 {
 		countLines(os.Stdin, counts)
-	} else {
-		for _, arg := range files {
-			f, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
-				continue
-			}
+		} else {
+			for _, arg := range files {
+				f, err := os.Open(arg)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+					continue
+				}
 			countLines(f, counts)
 			f.Close()
 		}
